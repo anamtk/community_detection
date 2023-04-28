467b0ffdb31875dc2fa87493bc21cb300a36df5f
an-bui <a.thanh.bui@gmail.com>
2023-04-28 15:58:03 -0700

    creating a metadata file

---
+++ code/02_data_prep/fish/fish_metadata.R
@@ -0,0 +1,29 @@
+# metadata
+# An Bui
+# April 2023
+
+
+# Load packages -----------------------------------------------------------
+
+
+package.list <- c("here", "tidyverse", "janitor")
+
+## Installing them if they aren't already on the computer
+new.packages <- package.list[!(package.list %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+## And loading them
+for(i in package.list){library(i, character.only = T)}
+
+
+# read csv ----------------------------------------------------------------
+
+fish_species <- read_csv(here::here("data_raw", "SBCLTER_Species_List_Master_20210113.csv")) %>% 
+  clean_names() %>% 
+  filter(group == "FISH")
+
+#Export that matrix to a central location for all matrices
+saveRDS(fish_species, here("data_outputs",
+                     "metadata",
+                     "species_metadata.RDS"))
+
