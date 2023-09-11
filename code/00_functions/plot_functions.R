845c77be4dd318526f1a0c204ac55d7e9b3e79ea
Ana Miller-ter Kuile <ana.miller.ter.kuile@gmail.com>
2023-09-11 14:08:36 -0700

    added plot functions script

---
+++ code/00_functions/plot_functions.R
@@ -0,0 +1,42 @@
+# Load packages ---------------------------------------------------------------
+
+# Load packages, here and tidyverse for coding ease, 
+package.list <- c("here", "tidyverse") 
+
+
+## Installing them if they aren't already on the computer
+new.packages <- package.list[!(package.list %in% 
+                                 installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+## And loading them
+for(i in package.list){library(i, character.only = T)}
+
+
+# Rhat graph --------------------------------------------------------------
+
+
+rhat_graph_fun <- function(list){
+
+  #this creates a dtaaframe out of the Rhat values from the model
+  df <- data.frame(id = names(list),
+                   Rhat = unlist(lapply(list, paste, collapse = ","))) %>%
+    #splits Rhat by , when that list element had more than one value
+    mutate(Rhat = str_split(Rhat, ",")) %>%
+    #unnests - so makes each Rhat a new row in the df
+    unnest(c(Rhat)) %>%
+    #make sure Rhat is a numeric
+    mutate(Rhat = as.numeric(Rhat)) 
+  
+  #plot histogram and make sure all below 1.1
+  plot <- ggplot(df, aes(x = Rhat)) +
+    geom_histogram() +
+    geom_vline(xintercept = 1.1, linetype = 2) +
+    theme_bw() +
+    scale_y_sqrt() +
+    #this is facetted by the parameter ID so you
+    # can find problematic parameters
+    facet_wrap(~ id)
+  
+  return(plot)
+}
--- code/00_functions/tidy_functions.R
+++ code/00_functions/tidy_functions.R
@@ -1,3 +1,17 @@
+# Load packages ---------------------------------------------------------------
+
+# Load packages, here and tidyverse for coding ease, 
+package.list <- c("here", "tidyverse") 
+
+
+## Installing them if they aren't already on the computer
+new.packages <- package.list[!(package.list %in% 
+                                 installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+## And loading them
+for(i in package.list){library(i, character.only = T)}
+
 
 
 # Extract Chlrophyl for each point ----------------------------------------
