8e080815a915343edcd339497b818d773a45393b
slamm1 <sml665@nau.edu>
2023-09-25 17:13:02 -0700

    set up and progress on nps data

---
+++ 04_nps_plants/code/01_nps_data_prep/02_test_model.R
@@ -0,0 +1,66 @@
+# Running the MSOM for birds
+# Ana Miller-ter Kuile
+# September 8, 2023
+
+# Load packages ---------------------------------------------------------------
+
+# Load packages, here and tidyverse for coding ease, 
+package.list <- c("here", "tidyverse", 
+                  "jagsUI",
+                  'rjags',
+                  'mcmcplots',
+                  "coda") #mcmc output
+
+
+## Installing them if they aren't already on the computer
+new.packages <- package.list[!(package.list %in% 
+                                 installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+## And loading them
+for(i in package.list){library(i, character.only = T)}
+
+
+# Load Data ---------------------------------------------------------------
+
+data <- readRDS(here('04_nps_plants',
+                     'example',
+                     'data_outputs',
+                     'model_inputs',
+                     'JAGS_data_list.RDS'))
+
+# Parameters to save ------------------------------------------------------
+
+params <- c(
+  'a1.Effort',
+  'lambda.mean',
+  'sig.lambda',
+  'a0.mean',
+  'sig.a0')
+
+
+#ideally you would use the omega.init object to set inits, but it 
+#seems to break something with this dataset in particular,
+#let me know if this is happening to you and we can troubleshoot
+inits <- function() list(omega = omega.init)
+
+# JAGS model --------------------------------------------------------------
+
+model <- here("04_nps_plants",
+              "example", 
+              'code',
+              'jags',
+              "kb_dyn_MSOM.R")
+
+Sys.time()
+mod <- jagsUI::jags(data = data,
+                    #inits = inits,
+                    inits = NULL,
+                    model.file = model,
+                    parameters.to.save = params,
+                    parallel = TRUE,
+                    n.chains = 3,
+                    n.iter = 1,
+                    DIC = TRUE)
+
+Sys.time()
