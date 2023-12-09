426c35707e7a0b1543d8cff8edd21c13761e1618
an-bui <a.thanh.bui@gmail.com>
2023-12-08 17:59:42 -0800

    new option for figure 1

--- 05_visualizations/code/detection_partialplots.R
+++ 05_visualizations/code/detection_partialplots.R
@@ -56,22 +56,38 @@ plant_sum <- readRDS(here('04_nps_plants',
 
 fish_effects <- as.data.frame(fish_sum$quantiles) %>%
   rownames_to_column(var = "parm") %>%
-  filter(parm %in% c("a1.Vis", "a2.Size"))
+  filter(parm %in% c("a1.Vis", "a2.Size")) %>% 
+  mutate(parm = case_match(parm,
+    "a1.Vis" ~ "Dive visibility",
+    "a2.Size" ~ "Fish size"
+  ))
 
-(fishdetect <- ggplot(fish_effects, aes(x = parm, y = `50%`)) +
-  geom_hline(yintercept = 0, linetype = 2, linewidth = 0.75) +
+fishdetect <- ggplot(fish_effects, aes(x = `50%`, y = parm)) +
+  geom_vline(xintercept = 0, linetype = 3, size = 0.75, alpha = 0.4) +
   geom_point(size = 2) +
-  geom_errorbar(aes(ymin = `2.5%`, ymax = `97.5%`), size = 0.75, width = 0) +
-  labs(x = "Detection covariate",
-       y = "Covariate effect \n (Median and 95% BCI)",
-       title = "SBC fish") +
-  scale_x_discrete(labels = c("Dive visibility", "Fish size")) + 
-  coord_flip() +
-  theme(axis.text = element_text(size = 12),
-        axis.title= element_text(size = 15),
+  geom_errorbar(aes(xmin = `2.5%`, xmax = `97.5%`), size = 0.75, width = 0, alpha = 0.6) +
+  scale_x_continuous(limits = c(-3.25, 3.25), breaks = c(-3, -2, -1, 0, 1, 2, 3)) +
+  labs(x = "Covariate effect",
+       y = "SBC fish",
+       title = "(a) SBC fish") +
+  # coord_flip() +
+  theme(axis.text = element_text(size = 6),
+        axis.title = element_text(size = 7),
         axis.title.y = element_blank(),
-        plot.title.position = "panel",
-        plot.title = element_text(hjust = 0.5)))
+        # axis.title.y = element_text(angle = 0, vjust = 0.5),
+        plot.title.position = "plot",
+        plot.title = element_text(size = 8),
+        panel.grid = element_blank(),
+        axis.ticks.y = element_blank(),
+        panel.border = element_blank()) +
+  geom_hline(yintercept = 0.4)
+
+fishdetect
+
+fish_title <- ggplot(data.frame(l = "SBC fish", x = 1, y = 1)) +
+  geom_text(aes(x, y, label = l), size = 4.5) + 
+  theme_void() +
+  coord_cartesian(clip = "off")
 
 # ggsave(plot = fishdetect,
 #        filename = here('pictures',
@@ -84,87 +100,128 @@ fish_effects <- as.data.frame(fish_sum$quantiles) %>%
 
 # Birds -------------------------------------------------------------------
 
-
-
 bird_effects <- as.data.frame(bird_sum$quantiles) %>%
   rownames_to_column(var = "parm") %>%
-  filter(parm %in% c("a1.Effort", "a2.Size"))
+  filter(parm %in% c("a1.Effort", "a2.Size")) %>% 
+  mutate(parm = case_match(parm,
+    "a1.Effort" ~ "Survey effort",
+    "a2.Size" ~ "Bird size"
+  ))
 
-(birddetect <- ggplot(bird_effects, aes(x = parm, y = `50%`)) +
-    geom_hline(yintercept = 0, linetype = 2, size = 0.75) +
-    geom_point(size = 2) +
-    geom_errorbar(aes(ymin = `2.5%`, ymax = `97.5%`), size = 0.75, width = 0) +
-    labs(x = "Detection covariate",
-         y = "Covariate effect \n (Median and 95% BCI)",
-         title = "KNZ birds") +
-    scale_x_discrete(labels = c("Survey effort", "Bird size")) + 
-    coord_flip() +
-    theme(axis.text = element_text(size = 12),
-          axis.title= element_text(size = 15),
-          plot.title.position = "panel",
-          plot.title = element_text(hjust = 0.5)))
+birddetect <- ggplot(bird_effects, aes(x = `50%`, y = parm)) +
+  geom_vline(xintercept = 0, linetype = 3, size = 0.75, alpha = 0.4) +
+  geom_point(size = 2) +
+  geom_errorbar(aes(xmin = `2.5%`, xmax = `97.5%`), size = 0.75, width = 0, alpha = 0.6) +
+  scale_x_continuous(limits = c(-1.6, 1.6), breaks = c(-1.5, -1, -0.5, 0, 0.5, 1, 1.5)) +
+  labs(x = "Covariate effect",
+       y = "",
+       title = "(b) KNZ birds") +
+  theme(axis.text = element_text(size = 6),
+        axis.title = element_text(size = 7),
+        axis.title.y = element_blank(),
+        # axis.title.y = element_text(angle = 0, vjust = 0.5),
+        plot.title.position = "plot",
+        plot.title = element_text(size = 8),
+        panel.grid = element_blank(),
+        panel.border = element_blank(),
+        axis.ticks.y = element_blank()) +
+  geom_hline(yintercept = 0.4)
+
+birddetect
+
+bird_title <- ggplot(data.frame(l = "KNZ birds", x = 1, y = 1)) +
+  geom_text(aes(x, y, label = l), size = 4.5) + 
+  theme_void() +
+  coord_cartesian(clip = "off")
 
 
 # Plants ------------------------------------------------------------------
 
-
 plant_effects <- as.data.frame(plant_sum$quantiles) %>%
   rownames_to_column(var = "parm") %>%
-  filter(str_detect(parm, "a1.Cover|a2.LifeGroup"))
+  filter(str_detect(parm, "a1.Cover|a2.LifeGroup")) %>% 
+  mutate(new_parm = case_match(
+    parm,
+    "a1.Cover" ~ "Average\nspecies\ncover", 
+    "a2.LifeGroup[2]" ~ "Perennial forb",
+    "a2.LifeGroup[3]" ~ "Perennial grass",
+    "a2.LifeGroup[4]" ~ "Perennial shrub",
+    "a2.LifeGroup[5]" ~ "Annual grass",
+    "a2.LifeGroup[6]" ~ "Biennial forb",
+    "a2.LifeGroup[7]" ~ "Perennial cacti",
+    "a2.LifeGroup[8]" ~ "Perennial succulent"
+  ),
+  new_parm = fct_inorder(new_parm)) 
 
 plant_effects1 <- plant_effects %>%
   filter(parm == "a1.Cover")
 
-(plantdetect1 <- ggplot(plant_effects1, aes(x = parm, y = `50%`)) +
-    geom_hline(yintercept = 0, linetype = 2, size = 0.75) +
-    geom_point(size = 2) +
-    geom_errorbar(aes(ymin = `2.5%`, ymax = `97.5%`), size = 0.75, width = 0) +
-    labs(x = "Detection covariate",
-         y = "Covariate effect \n (Median and 95% BCI)",
-         title = "PFNP Plants") +
-    scale_x_discrete(labels = c("Average species cover")) + 
-    coord_flip() +
-    theme(axis.text = element_text(size = 12),
-          axis.title= element_text(size = 15),
-          axis.title.y = element_blank(),
-          plot.title.position = "panel",
-          plot.title = element_text(hjust = 0.5)))
+plantdetect1 <- ggplot(plant_effects1, aes(x = `50%`, y = new_parm)) +
+  geom_vline(xintercept = 0, linetype = 3, size = 0.75, alpha = 0.4) +
+  geom_point(size = 2) +
+  geom_errorbar(aes(xmin = `2.5%`, xmax = `97.5%`), size = 0.75, width = 0) +
+  scale_x_continuous(limits = c(-1.75, 1.75), breaks = c(-1.5, -1, -0.5, 0, 0.5, 1, 1.5)) +
+  labs(x = "Covariate effect",
+       y = "",
+       title = "(c) PFNP plants: average species cover") +
+  theme(axis.text = element_text(size = 6),
+        axis.title = element_text(size = 7),
+        axis.title.y = element_blank(),
+        # axis.title.y = element_text(angle = 0, vjust = 0.5),
+        plot.title.position = "plot",
+        plot.title = element_text(size = 8),
+        panel.grid = element_blank(),
+        panel.border = element_blank(),
+        axis.ticks.y = element_blank()) +
+  geom_hline(yintercept = 0.4)
+
+plantdetect1
 
 plant_effects2 <- plant_effects %>%
   filter(str_detect(parm, "a2")) %>%
   filter(parm != "a2.LifeGroup[1]")
 
-(plantdetect2 <- ggplot(plant_effects2, aes(x = parm, y = `50%`)) +
-    geom_hline(yintercept = 0, linetype = 2, size = 0.75) +
-    geom_point(size = 2) +
-    geom_errorbar(aes(ymin = `2.5%`, ymax = `97.5%`), size = 0.75, width = 0) +
-    labs(x = "Life group",
-         y = "Covariate effect \n (Median and 95% BCI)") +
-    scale_x_discrete(labels = c("Perennial forb", "Perennial grass", 'Perennial shrub',
-                                'Annual grass', 'Biennial forb', 'Perennial cacti',
-                                'Perennial succulent')) +
-    theme(axis.text = element_text(size = 12),
-          axis.title= element_text(size = 15),
-          axis.text.x = element_text(angle = 45, hjust = 1),
-          plot.title.position = "panel",
-          plot.title = element_blank()) )
+plantdetect2 <- ggplot(plant_effects2, aes(x = `50%`, y = new_parm)) +
+  geom_vline(xintercept = 0, linetype = 3, size = 0.75, alpha = 0.4) +
+  geom_point(size = 2) +
+  geom_errorbar(aes(xmin = `2.5%`, xmax = `97.5%`), size = 0.75, width = 0) +
+  scale_x_continuous(limits = c(-10, 40)) +
+  labs(x = "Covariate effect",
+       y = "",
+       title = "(d) PFNP plants: Life group") +
+  theme(axis.text = element_text(size = 6),
+        axis.title = element_text(size = 7),
+        axis.title.y = element_blank(),
+        # axis.title.y = element_text(angle = 0, vjust = 0.5),
+        plot.title.position = "plot",
+        plot.title = element_text(size = 8),
+        panel.grid = element_blank(),
+        panel.border = element_blank(),
+        axis.ticks.y = element_blank()) +
+  geom_hline(yintercept = 0.4)
 
-plants <- plantdetect1 + plantdetect2
+plantdetect2
+
+plant_title <- ggplot(data.frame(l = "PFNP plants", x = 1, y = 1)) +
+  geom_text(aes(x, y, label = l), size = 6) + 
+  theme_void() +
+  coord_cartesian(clip = "off")
 
 # String them together ----------------------------------------------------
 
-#I NEED HELP MAKING THESE PRETTTTHHHH
+detect_together <- (fishdetect)/(birddetect)/(plantdetect1)/(plantdetect2) +
+  plot_layout(heights = c(10, 10, 10, 10))
 
-fishdetect / birddetect/ plants +
-  plot_annotation(tag_levels = "A")
+detect_together
 
-ggsave(plot = last_plot(),
+ggsave(plot = detect_together,
        filename = here("pictures",
                        "detection_models",
                        "detection_covariate_effects.jpg"),
-       height = 9,
-       width = 8,
-       units = "in")
+       height = 14,
+       width = 10,
+       units = "cm",
+       dpi = 300)
 
 # Old code for partial plots ----------------------------------------------
 
